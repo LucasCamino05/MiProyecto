d789d2435912b6d6f242094613522a419fc37a6a
Lucas Camino <lucascamino05@gmail.com>
2022-03-01 23:25:33 -0300

    Nombre

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css';
 export const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Navbar</a>
+      <a className="navbar-brand" href="#">Como en casa</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -11,7 +11,7 @@ export const NavBar = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Inicio <span className="sr-only"></span></a>
+            <a className="nav-link" href="#" >Inicio <span className="sr-only"></span></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Menú</a>
