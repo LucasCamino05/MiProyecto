e4df38a4a53fe2f5d15d4a4a14e7b61de2a88597
Lucas Camino <lucascamino05@gmail.com>
2022-04-13 20:24:06 -0300

    Trabajo de css

--- src/component/Item/Item.jsx
+++ src/component/Item/Item.jsx
@@ -1,6 +1,5 @@
 import { React } from "react";
 import { Link } from 'react-router-dom';
-import { ItemCount } from '../ItemCount/ItemCount';
 import { motion } from 'framer-motion';
 import './Item.css'
 
@@ -9,10 +8,10 @@ export const Item = ({id, nombre, imagen, descripcion, stock}) => {
         <>
             <motion.div className='container-fluid' animate={{y: 50}} transition={{duration: 1}}>            
                 <div className="itemCards">
-                    <div className="Prod-Div">
+                    <div className="Prod__Div">
                         <img src={imagen} className="card-img-top" alt="Not Found"/>
                     </div>
-                    <div className="card-body CardBody">
+                    <div className="CardBody">
                         <h1 style={{textDecoration: 'underline'}}>{nombre}</h1>
                         <p className="card-text">{descripcion}</p>
                         <Link to={`/detail/${id}`}> <button type="button" className="btn btn-primary btn-large" style={{margin: '5px'}}> Detalles </button></Link>
